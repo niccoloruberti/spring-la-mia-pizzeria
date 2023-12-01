e4c0df7b266537881781fdc51e9d3d5a4a744b70
niccoloruberti <nikiruberti@gmail.com>
2023-12-01 14:57:42 +0100

    aggiunta rotta pagina singola pizza

--- src/main/java/org/java/spring/controller/MainController.java
+++ src/main/java/org/java/spring/controller/MainController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class MainController {
@@ -23,5 +24,14 @@ public class MainController {
 		model.addAttribute("pizzas", pizzas);
 		
 		return "pizzas";
+	}
+	
+	@GetMapping("/pizzas/{id}")
+	public String getPizza(Model model, @PathVariable int id) {
+		
+		Pizza pizza = pizzaService.findById(id);
+		model.addAttribute("pizza", pizza);
+		
+		return "showPizza";
 	}
 }
