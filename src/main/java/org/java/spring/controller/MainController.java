bd8d661166f6cf6db9dc4cfd07f3abfa4cf05b28
niccoloruberti <nikiruberti@gmail.com>
2023-12-04 14:52:47 +0100

    es completato

--- src/main/java/org/java/spring/controller/MainController.java
+++ src/main/java/org/java/spring/controller/MainController.java
@@ -7,9 +7,14 @@ import org.java.spring.db.serv.PizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class MainController {
@@ -48,6 +53,21 @@ public class MainController {
 		model.addAttribute("pizza", pizza);
 		
 		return "createPizza";
+	}
+	
+	@PostMapping("/create")
+	public String store(@Valid @ModelAttribute("pizza") Pizza formPizza,
+		BindingResult bindingResult,
+		Model model) {
+		
+		if(bindingResult.hasErrors()) {
+			return "createPizza";
+		}
+		
+		pizzaService.save(formPizza);
+		
+		return "redirect:/";
+		
 	}
 	
 }
--- src/main/java/org/java/spring/db/pojo/Pizza.java
+++ src/main/java/org/java/spring/db/pojo/Pizza.java
@@ -1,10 +1,14 @@
 package org.java.spring.db.pojo;
 
+import org.hibernate.validator.constraints.Length;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 public class Pizza {
@@ -13,14 +17,22 @@ public class Pizza {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
+	@NotEmpty(message = "Il nome non può essere vuoto")
+	@Length(min = 5, max = 60, message = "Il nome deve essere almeno 5 caratteri e massimo 60")
 	private String nome;
-
+	
+	@NotEmpty(message = "La descrizione non può essere vuota")
 	@Column(columnDefinition = "TEXT")
+	@Length(min = 5, max = 300, message = "La descrizione deve essere almeno 5 caratteri e massimo 300")
 	private String descrizione;
 	
+	@NotEmpty(message = "Il link dell'immagine non può essere vuoto")
 	@Column(columnDefinition = "TEXT")
+	@Length(max = 300, message = "Il link immagine non può superare i 300 caratteri")
 	private String foto;
 	
+	@NotNull(message = "Il prezzo non può essere vuoto")
+	@Min(value = 1, message = "Il prezzo deve essere maggiore o uguale a 1")
 	private Double prezzo;
 	
 	public Pizza() {};
